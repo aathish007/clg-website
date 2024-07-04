02d4b654d3c10363a998b8507f06a748fd2b9abd
aathish007 <mailforaathish@gmail.com>
2024-07-04 10:15:47 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationComponet from './Components/NavigationComponet/NavigationComponet'
+import FooterComponent from './Components/FooterComponent/FooterComponent'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <NavigationComponet/>
+      <FooterComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
